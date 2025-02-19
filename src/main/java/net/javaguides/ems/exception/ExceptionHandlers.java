e03501865d2979af016db5b6337693b5ae3c00fc
HeartyTjan <salamitijani02@gmail.com>
2025-02-19 19:40:49 +0100

    Add Exception handler for Bad Request

--- src/main/java/net/javaguides/ems/exception/ExceptionHandlers.java
+++ src/main/java/net/javaguides/ems/exception/ExceptionHandlers.java
@@ -3,8 +3,14 @@ package net.javaguides.ems.exception;
 import com.sun.jdi.event.StepEvent;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandlers {
@@ -19,5 +25,19 @@ public class ExceptionHandlers {
     public ResponseEntity<String> handleEmployeeAlreadyExistException(EmployeeAlreadyExistException ex) {
         String message = ex.getMessage();
         return new ResponseEntity<>(message, HttpStatus.CONFLICT);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String > handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 }
